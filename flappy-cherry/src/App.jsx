0844c8e7c242c7da81791203f3a04ce3e539ee53
AgneDJ <fizikee@gmail.com>
2024-04-05 00:10:12 -0700

    Made cherry image visible in front of  background

--- flappy-cherry/src/App.jsx
+++ flappy-cherry/src/App.jsx
@@ -4,11 +4,10 @@ import Game from "./flappy_cherry_components/start_game";
 function App() {
   return (
     <div className="App">
-      <img src="cherry.webp" className="App-logo" alt="logo" />
+      <img src="../assets/images/cherry.webp" className="App-logo" alt="logo" />
+
       <h1>Flappy Cherry</h1>
-      {/* <header className="App-header"> */}
       <Game />
-      {/* </header> */}
     </div>
   );
 }
--- flappy-cherry/src/flappy_cherry_components/cherry.jsx
+++ flappy-cherry/src/flappy_cherry_components/cherry.jsx
@@ -3,15 +3,21 @@ import "../App.css";
 
 const Cherry = ({ y, r }) => {
   return (
-    <div
-      className="Cherry"
-      style={{
-        top: y,
-        left: 120,
-        transform: `rotate(${r}deg)`,
-        transition: "transform 100ms, top 300ms",
-      }}
-    ></div>
+    <div>
+      <img
+        src="../assets/images/flying_cherry.webp"
+        className="Cherry"
+        alt="flying_cherry"
+        style={{
+          top: y,
+          left: 120,
+          transform: `rotate(${r}deg)`,
+          transition: "transform 100ms, top 300ms",
+          height: 50,
+          width: 50,
+        }}
+      />
+    </div>
   );
 };
 
