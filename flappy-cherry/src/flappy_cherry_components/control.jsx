2f1d77c274a2e510d6b99fb31c24fbc2a6383447
AgneDJ <fizikee@gmail.com>
2024-04-12 14:30:54 -0700

    Included gravity to a game

--- flappy-cherry/src/flappy_cherry_components/control.jsx
+++ flappy-cherry/src/flappy_cherry_components/control.jsx
@@ -1,63 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Cherry from "./cherry";
 import "../App.css";
 
 const Control = () => {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: 100, y: 100 });
+  const [velocity, setVelocity] = useState({ x: 0, y: 0 });
 
-  const moveObject = (direction) => {
-    setPosition((prevPosition) => {
-      switch (direction) {
-        case "left":
-          if (prevPosition.x <= 0) {
-            return { ...prevPosition, x: 0 };
-          } else {
-            return { ...prevPosition, x: prevPosition.x - 10 };
-          }
+  useEffect(() => {
+    const gravity = 0.2;
+    const friction = 0.2;
 
-        case "right":
-          if (prevPosition.x >= 650) {
-            return { ...prevPosition, x: 650 };
-          } else {
-            return { ...prevPosition, x: prevPosition.x + 50 };
-          }
+    const updatePosition = () => {
+      const newVelocity = { ...velocity, y: velocity.y + gravity };
 
-        case "up":
-          if (prevPosition.y <= 0) {
-            return { ...prevPosition, y: 0 };
-          } else {
-            return { ...prevPosition, y: prevPosition.y - 50 };
-          }
-        case "down":
-          if (prevPosition.y >= 350) {
-            return { ...prevPosition, y: 350 };
-          } else {
-            return { ...prevPosition, y: prevPosition.y + 10 };
-          }
-        default:
-          return prevPosition;
+      const newVelocityWithFriction = {
+        x: velocity.x * friction,
+        y: newVelocity.y,
+      };
+
+      const newPosition = {
+        x: position.x + newVelocityWithFriction.x,
+        y: position.y + newVelocityWithFriction.y,
+      };
+      //Boundary check
+      if (newPosition.x < 0) {
+        newPosition.x = 0;
+      } else if (newPosition.x > 700) {
+        newPosition.x = 650;
       }
-    });
+
+      if (newPosition.y < 0) {
+        newPosition.y = 0;
+        setVelocity({ ...newVelocityWithFriction, y: 0 });
+      } else if (newPosition.y > 370) {
+        newPosition.y = 370;
+        setVelocity({ ...newVelocityWithFriction, y: 0 });
+      }
+
+      setPosition(newPosition);
+      setVelocity(newVelocityWithFriction);
+    };
+
+    const gameLoop = setInterval(() => {
+      updatePosition();
+    }, 1000 / 60);
+
+    return () => clearInterval(gameLoop);
+  }, [position, velocity]);
+
+  const fly = () => {
+    setVelocity({ ...velocity, y: -10 });
   };
+
   return (
     <div>
-      <div style={{ position: "relative" }}>
-        <button onClick={() => moveObject("left")}>Left</button>
-        <button onClick={() => moveObject("right")}>Right</button>
-        <button onClick={() => moveObject("up")}>Up</button>
-        <button onClick={() => moveObject("down")}>Down</button>
-      </div>
       <div
         style={{
           position: "relative",
           left: position.x + "px",
           top: position.y + "px",
+          transition: "left 0.1s, top 0.1s",
           zIndex: 2,
-          transition: "left 0.3s, top 0.3s",
         }}
       >
         <Cherry />
       </div>
+      <button onClick={fly}>Fly</button>
     </div>
   );
 };
