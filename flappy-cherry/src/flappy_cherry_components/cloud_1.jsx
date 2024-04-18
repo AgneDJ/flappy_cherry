488cf0a75022e1d0d6b90149df021c29b2c1bcd2
AgneDJ <fizikee@gmail.com>
2024-04-17 17:20:39 -0700

    # 55 Added Lilly to the cloud

--- flappy-cherry/src/flappy_cherry_components/cloud_1.jsx
+++ flappy-cherry/src/flappy_cherry_components/cloud_1.jsx
@@ -3,12 +3,20 @@ import "../App.css";
 
 function Cloud({ style }) {
   return (
-    <img
-      src="../assets/images/cloud.png"
-      className="Cloud"
-      alt="just a cloud"
-      style={style}
-    />
+    <div>
+      <img
+        src="../assets/images/cloud.png"
+        className="Cloud"
+        alt="just a cloud"
+        style={style}
+      />
+      <img
+        src="../assets/images/lilly.png"
+        className="Lilly"
+        alt="Lilly the cat"
+        style={style}
+      />
+    </div>
   );
 }
 
