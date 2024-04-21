ddbb15cfc88cdb74c7f339c5b5b50840aba8c315
AgneDJ <fizikee@gmail.com>
2024-04-20 23:47:44 -0700

    Added cloud position tracking

--- flappy-cherry/src/flappy_cherry_components/PositionContext.jsx
+++ flappy-cherry/src/flappy_cherry_components/PositionContext.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Control from "./control";
+import NewCloudGen from "./newCloudGen";
 
 function DisplayInfo({ position }) {
   return (
     <div>
-      Current Position: X: {position.x}, Y: {position.y}
+      <div>
+        Current Position: X: {position.x}, Y: {position.y}
+      </div>
+      <div>
+        <h3>Cloud Positions:</h3>
+        {position.map((pos, index) => (
+          <div key={index}>
+            Cloud {index}: X: {pos.right}%, Y: {pos.top}%
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
