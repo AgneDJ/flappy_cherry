e9151dac7d3cabc2604f80a5c704d8f3386f449a
AgneDJ <fizikee@gmail.com>
2024-04-23 17:05:05 -0700

    #68 added test file

--- flappy-cherry/src/setupTests.js
+++ flappy-cherry/src/setupTests.js
@@ -1,5 +1,18 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+// Control.test.js
+import "@testing-library/jest-dom";
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import Control from ".src/flappy_cherry_components/control";
+
+// describe("Counter component", () => {
+//   test("initializes count at 0 and increments by 1 when button is clicked", () => {
+//     render(<Counter />);
+//     const countElement = screen.getByTestId("count");
+//     expect(countElement).toHaveTextContent("0"); // Check initial value
+
+//     const incrementButton = screen.getByText("Increment");
+//     fireEvent.click(incrementButton);
+
+//     expect(countElement).toHaveTextContent("1"); // Check incremented value
+//   });
+// });
