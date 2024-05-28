357d1c54b19fbd9f47fdb0d0a4bc9712bfedfd42
AgneDJ <fizikee@gmail.com>
2024-05-28 13:13:42 -0700

    Added component discription comment

--- flappy-cherry/src/flappy_cherry_components/cherry.jsx
+++ flappy-cherry/src/flappy_cherry_components/cherry.jsx
@@ -1,10 +1,12 @@
 import React, { useRef, useEffect } from "react";
 import "../App.css";
 
+// Creates Cherry component
 function Cherry() {
   return (
+    // Returns cherry image
     <img
-      src="../assets/images/flying_cherry.png"
+      src="../assets/images/flying_cherry.webp"
       className="Cherry"
       alt="flying_cherry"
     />
