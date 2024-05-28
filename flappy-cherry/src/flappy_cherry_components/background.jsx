62a3a7b6da6c92e18697a35b18ea947a4e1d57d4
AgneDJ <fizikee@gmail.com>
2024-05-28 12:58:30 -0700

    Added comment for the function

--- flappy-cherry/src/flappy_cherry_components/background.jsx
+++ flappy-cherry/src/flappy_cherry_components/background.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import "../App.css";
 
+// Displays image as game background
 function Background() {
   return <div className="gameBackground"></div>;
 }
