b55b8d2820cf909f57232d9e03d0b3f195c7a114
AgneDJ <fizikee@gmail.com>
2024-05-29 15:33:31 -0700

    Added some comments and skeleton with some pseudocode for game over component

---
+++ flappy-cherry/src/flappy_cherry_components/game_over.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from "react";
+import Game from "./flappy_cherry_components/start_game";
+import Cherry from "./cherry";
+import Cloud from "./cloud_1";
+import DisplayInfo from "./PositionContext";
+import "../App.css";
+
+const Over = () => {
+  //when collision happens: stop the game, show score table, show 'game over'
+
+  // get current cherry position;
+  // get any generated cloud position;
+  // check if current cherry position overlaps cloud position
+  // if yes: stop Game
+  // if no: continue checking
+  // ---checking and game over could be two separate components
+
+  return;
+};
+
+export default Over;
